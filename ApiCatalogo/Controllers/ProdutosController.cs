cf91371e3a09ac7665aba8dd808dceb3d5c7e177
Felipe Muniz <lipe.baterra@gmail.com>
2022-07-07 12:03:56 -0300

    Criando o controlador da classe produto

---
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -0,0 +1,17 @@
+﻿using ApiCatalogo.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProdutosController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public ProdutosController(AppDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
