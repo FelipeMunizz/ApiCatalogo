41dc53983b9d1266e0a9ae9b30a191d23e77ca57
Felipe Muniz <lipe.baterra@gmail.com>
2022-07-07 21:24:21 -0300

    Concluimos a implementação da Controller de produtos

--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -1,6 +1,8 @@
 ﻿using ApiCatalogo.Data;
+using ApiCatalogo.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ApiCatalogo.Controllers
 {
@@ -12,6 +14,74 @@ namespace ApiCatalogo.Controllers
         public ProdutosController(AppDbContext context)
         {
             _context = context;
+        }
+
+        [HttpGet]
+        //Motivos por usar interface de enumerable ao inves de List<>
+        //1° interface somente para leitura,
+        //2° enumerable trabalha por demanda,
+        //3°não necessito ter toda coleção na memória
+        public ActionResult<IEnumerable<Produto>> Get()
+        {
+            var produtos = _context.Produtos.ToList();
+            if (produtos is null)
+            {
+                return NotFound("Produtos não encontrados");
+            }
+            return produtos;
+        }
+
+        [HttpGet("{id:int}", Name= "ObterProduto")]
+        public ActionResult<Produto> Get(int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+            if(produto is null)
+            {
+                return NotFound("Produto não Encontrado");
+            }
+            return produto;
+        }
+
+        [HttpPost]
+        public ActionResult Post (Produto produto)
+        {
+            if (produto is null)
+                return BadRequest("Não foi possivel adicionar o produto");
+
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterProduto",
+                new { id = produto.ProdutoId }, produto);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put (int id, Produto produto)
+        {
+            if (id != produto.ProdutoId)
+            {
+                return BadRequest();
+            }
+                _context.Entry(produto).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok(produto);            
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete (int id)
+        {
+            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+            if(produto is null)
+            {
+                return NotFound("Produto não localizado.");
+            }
+
+            _context.Produtos.Remove(produto);
+            _context.SaveChanges();
+
+            return Ok("Produto excluido com sucesso.");
         }
     }
 }
---
+++ ApiCatalogo/Migrations/20220707230249_CriacaoBdCasa.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiCatalogo.Migrations
+{
+    public partial class CriacaoBdCasa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
