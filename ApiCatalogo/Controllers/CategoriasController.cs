91a0121cb56d1824185b570c8faf1c431468b179
Felipe Muniz <lipe.baterra@gmail.com>
2022-07-07 22:10:49 -0300

    Criação da controller categoria e tratamos um erro de looping que acontecia devido o relacionamento das entidades

---
+++ ApiCatalogo/Controllers/CategoriasController.cs
@@ -0,0 +1,81 @@
+﻿using ApiCatalogo.Data;
+using ApiCatalogo.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+        public CategoriasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("produtos")]
+        public ActionResult<IEnumerable<Categoria>> GetCategoriasProdutos()
+        {
+            return _context.Categorias.Include(p => p.Produtos).ToList();
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Categoria>> Get()
+        {
+            return _context.Categorias.ToList();
+        }
+
+        [HttpGet("{id:int}", Name = "ObterCategoria")]
+        public ActionResult<Categoria> Get(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(p => p.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não Encontrado");
+            }
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        public ActionResult Post(Categoria categoria)
+        {
+            if (categoria is null)
+                return BadRequest("Não foi possivel adicionar o categoria");
+
+            _context.Categorias.Add(categoria);
+            _context.SaveChanges();
+
+            return new CreatedAtRouteResult("ObterCategoria",
+                new { id = categoria.CategoriaId }, categoria);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult Put(int id, Categoria categoria)
+        {
+            if (id != categoria.CategoriaId)
+            {
+                return BadRequest();
+            }
+            _context.Entry(categoria).State = EntityState.Modified;
+            _context.SaveChanges();
+
+            return Ok(categoria);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            var categoria = _context.Categorias.FirstOrDefault(c => c.CategoriaId == id);
+            if (categoria is null)
+            {
+                return NotFound("Categoria não encontrada.");
+            }
+            _context.Categorias.Remove(categoria);
+            _context.SaveChanges();
+            return Ok("Categoria excluida com sucesso");
+        }
+    }
+}
